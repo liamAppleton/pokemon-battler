46784f30cf7f680fa3a4882d358f6a977e15008b
Liam Appleton <liamappleton97@outlook.com>
2025-02-26 21:10:31 +0000

    adjusted timers

--- pokemon-battler/Game/index.js
+++ pokemon-battler/Game/index.js
@@ -79,8 +79,8 @@ function computerTurn() {
       borderLog(computer);
       return;
     } // computer win condition
-    setTimeout(() => round(), 400); // back to round
-  }, 400);
+    setTimeout(() => round(), 700); // back to round
+  }, 700);
 }
 
 function pokemonSelection() {
@@ -255,8 +255,8 @@ function round() {
             return;
           }
 
-          setTimeout(() => computerTurn(), 400);
-        }, 400);
+          setTimeout(() => computerTurn(), 700);
+        }, 700);
       }
     });
   }
